7d0df4f599823df33101d7a536badbf6bc157f20
Moushumi Rahman <Moushumi Rahman@DESKTOP-KDD88J4>
2019-10-09 20:19:22 -0700

    First change: completed add, subtract, toSting method

--- Assignment2/src/cse360assign2/AddingMachine.java
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -3,7 +3,7 @@
  * @author Moushumi Rahman
  * @classId CSE360 
  * @assignmentNumber 2
- *  
+ * @version 2 
  */
 
 package cse360assign2;
@@ -12,43 +12,55 @@ public class AddingMachine {
 	
 	private int total;
 	
+	private String fullString;
+	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		fullString = "0";
 	}
 	
 	/**
 	 * 
-	 * @return integer
+	 * @return integer which is the current total
 	 */
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
 	 * 
-	 * @param value
+	 * @param value which is added to the total
 	 */
 	
 	public void add (int value) {
 		
-	}
-	
-	/**
-	 * 
-	 * @param value
-	 */
-	
-	public void subtract (int value) {
+		total = total + value;
+		
+		fullString = fullString + " + " + value;
 		
 	}
 	
 	/**
-	 * @return String
+	 * 
+	 * @param value which is subtracted from the total
+	 */
+	
+	public void subtract (int value) {
+		
+		total = total - value;
+		
+		fullString = fullString + " - " + value;
+		
+	}
+	
+	/**
+	 * @return String which is the history
 	 */
 	
 	public String toString () {
-		return "";
+		
+		return fullString;
 	}
 
 	public void clear() {
