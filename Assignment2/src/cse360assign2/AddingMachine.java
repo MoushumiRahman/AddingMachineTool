d48ba3e7f5cbded976687488e14d1c551ec3a698
Moushumi Rahman <mourahman911@gmail.com>
2019-10-09 19:45:08 -0700

    Initial checking of AddingMachine tool.

---
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,59 @@
+/**
+ * This class works like a calculator.
+ * @author Moushumi Rahman
+ * @classId CSE360 
+ * @assignmentNumber 2
+ *  
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * 
+	 * @return integer
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * @return String
+	 */
+	
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+
+
+}
